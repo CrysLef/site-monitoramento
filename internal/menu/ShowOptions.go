6009832c396eaedbe76ef97595d56a3aee90b99d
Crystian <crystian.lf@gmail.com>
2023-10-21 09:13:46 -0300

    feat:Add function to show menu options

---
+++ internal/menu/ShowOptions.go
@@ -0,0 +1,10 @@
+package menu
+
+import "fmt"
+
+func ShowOptions() {
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Registrar um site")
+	fmt.Println("3- Exibir logs")
+	fmt.Println("0- Sair do programa")
+}
