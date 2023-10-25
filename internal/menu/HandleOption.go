7d327f5b7d5c19b86972250ce67bfef50103af81
Crystian <crystian.lf@gmail.com>
2023-10-24 22:10:41 -0300

    feat:Add registry website option

--- internal/menu/HandleOption.go
+++ internal/menu/HandleOption.go
@@ -11,14 +11,17 @@ func HandleOption() {
 
 	comando := ReadInput()
 	monitoring := site.Monitoring{}
+	websiteLogs := site.WebsiteLogs{}
 
 	switch comando {
 	case 1:
 		monitoring.Config()
 		monitoring.Start()
-
 	case 2:
+		site.RegistryWebsites()
+	case 3:
 		fmt.Println("Exibindo logs...")
+		websiteLogs.Read()
 	case 0:
 		fmt.Println("Saindo do programa...")
 		os.Exit(0)
