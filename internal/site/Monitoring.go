5ab7c3aabc5c296b4925956af980a73ef354f4ee
Crystian <crystian.lf@gmail.com>
2023-10-26 16:58:46 -0300

    feat:Add conditional if list of sites is empty

--- internal/site/Monitoring.go
+++ internal/site/Monitoring.go
@@ -14,22 +14,27 @@ func (m *Monitoring) Start() {
 	fmt.Println("Monitorando...")
 	sites := ReadWebsiteFile()
 
-	for i := 0; i < m.Monitoramentos; i++ {
-		for i, site := range sites {
-			fmt.Println("Testando site", i, ":", site)
-			IsRunning(site)
+	if sites[0] != "" {
+		for i := 0; i < m.Monitoramentos; i++ {
+
+			for i, site := range sites {
+				fmt.Println("Monitorando site", i+1, ":", site)
+				IsRunning(site)
+			}
+			time.Sleep(m.Delay * time.Second)
+			fmt.Println("")
 		}
-		time.Sleep(m.Delay * time.Second)
+		fmt.Println("")
+	} else {
+		fmt.Println("Sua lista de sites esta vazia, por favor registre pelo menos um site antes de realizar o monitoramento.")
 		fmt.Println("")
 	}
-	fmt.Println("")
 }
 
 func (m *Monitoring) Config() (int, time.Duration) {
 
 	for m.Monitoramentos == 0 || m.Delay == 0 {
-		fmt.Print("Digite quantos monitoramentos deseja: ")
-		fmt.Scan(&m.Monitoramentos)
+		fmt.Scanf("Digite quantos monitoramentos deseja: %d", &m.Monitoramentos)
 
 		if m.Monitoramentos == 0 {
 			fmt.Println("Ao menos 1 monitoramento deve ser feito")
@@ -40,8 +45,7 @@ func (m *Monitoring) Config() (int, time.Duration) {
 			break
 		}
 
-		fmt.Print("Digite o intervalo de tempo entre monitoramentos: ")
-		fmt.Scan(&m.Delay)
+		fmt.Scanf("Digite o intervalo de tempo entre monitoramentos: %d", &m.Delay)
 	}
 
 	return m.Monitoramentos, m.Delay
